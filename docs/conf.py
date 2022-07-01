e2d2ee7e0a4dd26bfca911532f3f5c3f866b702d
adamklie <aklie@ucsd.edu>
2022-06-30 18:52:44 -0700

    cleaning docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ release = '0.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['myst-parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -39,7 +38,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. eugene documentation master file, created by
-   sphinx-quickstart on Thu Jun 30 18:38:21 2022.
+   sphinx-quickstart on Thu Jun 30 18:50:27 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
