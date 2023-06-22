f00d321e6ce1b301cd2aa47f25a5746007edf14f
adamklie <aklie@ucsd.edu>
2023-06-22 16:24:59 -0700

    finished model instantation tutorial

--- eugene/models/__init__.py
+++ eugene/models/__init__.py
@@ -1,5 +1,6 @@
 from . import base
 from ._SequenceModule import SequenceModule
+from ._ProfileModule import ProfileModule
 from ._utils import load_config
 from ._utils import list_available_layers, get_layer
 from .base._initializers import init_motif_weights, init_weights
--- eugene/models/_utils.py
+++ eugene/models/_utils.py
@@ -19,15 +19,24 @@ def load_config(config_path, **kwargs):
         config_path = os.path.join(settings.config_dir, config_path)
     with open(config_path, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    module_name = config.pop("module")
-    model_params = config.pop("model")
-    arch_name = model_params["arch_name"]
-    arch = model_params["arch"]
-    model_type = getattr(importlib.import_module("eugene.models.zoo"), arch_name)
-    model = model_type(**arch)
-    module_type = getattr(importlib.import_module("eugene.models"), module_name)
-    module = module_type(model, **config, **kwargs)
-    return module
+    if "module" in config:
+        module_name = config.pop("module")
+        model_params = config.pop("model")
+        arch_name = model_params["arch_name"]
+        arch = model_params["arch"]
+        model_type = getattr(importlib.import_module("eugene.models.zoo"), arch_name)
+        model = model_type(**arch)
+        module_type = getattr(importlib.import_module("eugene.models"), module_name)
+        module = module_type(model, **config, **kwargs)
+        return module
+    elif "model" in config:
+        model_params = config.pop("model")
+        arch = model_params["arch"]
+        arch_name = model_params["arch_name"]
+        model_type = getattr(importlib.import_module("eugene.models.zoo"), arch_name)
+        model = model_type(**arch)
+    else:
+        raise ValueError("Config file must contain either a 'model' or 'module' key")
 
 
 def load_model(model_path):
